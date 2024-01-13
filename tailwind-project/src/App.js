ca24ba6c86196af11b39b610d369200ada776399
Charlie Newlinds <cnewlinds@gmail.com>
2024-01-13 11:06:31 +0700

    inital commit

---
+++ tailwind-project/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import SideBar from './components/sidebar';
+import './App.css';
+
+function App() {
+  return (
+    <div className='flex'>
+      <SideBar />
+    </div>
+  );
+}
+
+export default App;
