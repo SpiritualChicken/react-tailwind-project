f2a3688ee35c31b5f8426b161327bfb0cb776825
Charlie Newlinds <cnewlinds@gmail.com>
2024-03-24 18:45:06 +1100

    Display tattoo rework

--- tailwind-project/src/components/DisplayTattoo.jsx
+++ tailwind-project/src/components/DisplayTattoo.jsx
@@ -1,17 +1,28 @@
 import defaultTattoo from '../assets/tattooPlaceholder.png';
 import { CiHeart } from "react-icons/ci";
 import { IoIosAddCircleOutline } from "react-icons/io";
+import { FaUserCircle, } from 'react-icons/fa';
+
 
 
 
 function DisplayTattoo({ UserName, likes, saves }) {
     return (
       <div className='content-list ; '>
-        <div className="bg-white rounded-md shadow-md p-4 m-2  hover:cursor-pointer hover:transform hover:-translate-y-1 hover:-translate-x-1 transition-transform duration-300 ease-in-out "> 
+        <div className="bg-white rounded-md shadow-md p-4 m-2  hover:cursor-pointer hover:transform hover:-translate-y-1 hover:-translate-x-1 transition-transform duration-300 ease-in-out ">
+          <div className='block'>
+            <div className='flex items-center'>
+              <UserCircle className='mr-2' />    
+              <h4 className='font-semibold pl-1'>{`${ UserName }`}</h4>
+            </div>
+            <div>
+              <p className='text-sm'>Description</p>
+              <p className='text-sm'>#tag</p>
+            </div>
+          </div>
             <div className="displayTattooImgContainer"> 
                 <img src={defaultTattoo} alt={`${UserName}'s Tattoo`} className='w-full h-auto rounded-md'/>
             </div>
-            <h4 className='text-lg font-semibold mt-2 mb-1'>{`${ UserName }`}</h4>
             <div className='center flex'>
                 <Interaction icon={ <CiHeart />} displayNumber={likes}/>
                 <Interaction icon={ <IoIosAddCircleOutline /> } displayNumber={saves} /> 
@@ -29,5 +40,7 @@ function Interaction({ icon, displayNumber}) {
       </div>
     );
   }
+
+const UserCircle = () => <FaUserCircle size='24' />;
 
 export default DisplayTattoo
