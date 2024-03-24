9ca97be09af16734f29ffb99ea9897351fb171b6
Charlie Newlinds <cnewlinds@gmail.com>
2024-03-24 19:05:19 +1100

    rework display tattoo interaction

--- tailwind-project/src/components/DisplayTattoo.jsx
+++ tailwind-project/src/components/DisplayTattoo.jsx
@@ -2,6 +2,8 @@ import defaultTattoo from '../assets/tattooPlaceholder.png';
 import { CiHeart } from "react-icons/ci";
 import { IoIosAddCircleOutline } from "react-icons/io";
 import { FaUserCircle, } from 'react-icons/fa';
+import { FaRegComments } from "react-icons/fa";
+import { FaShare } from "react-icons/fa";
 
 
 
@@ -9,23 +11,25 @@ import { FaUserCircle, } from 'react-icons/fa';
 function DisplayTattoo({ UserName, likes, saves }) {
     return (
       <div className='content-list ; '>
-        <div className="bg-white rounded-md shadow-md p-4 m-2  hover:cursor-pointer hover:transform hover:-translate-y-1 hover:-translate-x-1 transition-transform duration-300 ease-in-out ">
+        <div className="bg-white rounded-md shadow-md hover:cursor-pointer hover:transform hover:-translate-y-1 hover:-translate-x-1 transition-transform duration-300 ease-in-out ">
           <div className='block'>
-            <div className='flex items-center'>
-              <UserCircle className='mr-2' />    
+            <div className='flex items-center pl-1 pt-1'>
+              <UserCircle />    
               <h4 className='font-semibold pl-1'>{`${ UserName }`}</h4>
             </div>
-            <div>
+            <div className='pl-1 pt-1 pb-1'>
               <p className='text-sm'>Description</p>
               <p className='text-sm'>#tag</p>
             </div>
           </div>
-            <div className="displayTattooImgContainer"> 
+            <div className="block border h-full w-full"> 
                 <img src={defaultTattoo} alt={`${UserName}'s Tattoo`} className='w-full h-auto rounded-md'/>
             </div>
-            <div className='center flex'>
+            <div className='flex justify-center pt-2 pb-1'>
                 <Interaction icon={ <CiHeart />} displayNumber={likes}/>
                 <Interaction icon={ <IoIosAddCircleOutline /> } displayNumber={saves} /> 
+                <Interaction icon={ <FaRegComments /> } displayNumber={saves} />
+                <Interaction icon={ <FaShare />} displayNumber={saves} />
             </div>
         </div>
       </div>
@@ -34,7 +38,7 @@ function DisplayTattoo({ UserName, likes, saves }) {
 
 function Interaction({ icon, displayNumber}) {
     return (
-      <div className="flex items-center mr-4">
+      <div className="flex items-center mr-4 content-container-icons">
         {icon}
         <div className="ml-2">{displayNumber}</div>
       </div>
