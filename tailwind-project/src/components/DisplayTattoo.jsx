25495366a275650b96167a5132f479f6a263c03f
Charlie Newlinds <cnewlinds@gmail.com>
2024-03-25 19:18:19 +1100

    remove comment and change icons

--- tailwind-project/src/components/DisplayTattoo.jsx
+++ tailwind-project/src/components/DisplayTattoo.jsx
@@ -12,7 +12,8 @@ function DisplayTattoo({ UserName, likes, saves }) {
     return (
       <div className='content-list ; '>
         <div className="bg-white rounded-md shadow-md hover:cursor-pointer hover:transform hover:-translate-y-1 hover:-translate-x-1 transition-transform duration-300 ease-in-out ">
-          <div className='block'>
+          {/* User name + Description */}
+          {/* <div className='block'>
             <div className='flex items-center pl-1 pt-1'>
               <UserCircle />    
               <h4 className='font-semibold pl-1'>{`${ UserName }`}</h4>
@@ -21,15 +22,17 @@ function DisplayTattoo({ UserName, likes, saves }) {
               <p className='text-sm'>Description</p>
               <p className='text-sm'>#tag</p>
             </div>
-          </div>
-            <div className="block border h-full w-full"> 
+          </div> */}
+            <div className="block border-2 rounded-md h-full w-full"> 
                 <img src={defaultTattoo} alt={`${UserName}'s Tattoo`} className='w-full h-auto rounded-md'/>
             </div>
             <div className='flex justify-center pt-2 pb-1'>
                 <Interaction icon={ <CiHeart />} displayNumber={likes}/>
                 <Interaction icon={ <IoIosAddCircleOutline /> } displayNumber={saves} /> 
-                <Interaction icon={ <FaRegComments /> } displayNumber={saves} />
-                <Interaction icon={ <FaShare />} displayNumber={saves} />
+                {/* Comment button */}
+                {/* <Interaction icon={ <FaRegComments /> } displayNumber={saves} /> */}
+                {/* Share Button */}
+                {/* <Interaction icon={ <FaShare />} displayNumber={saves} /> */}
             </div>
         </div>
       </div>
