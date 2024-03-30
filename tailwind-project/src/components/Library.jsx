5a109a1593f178914fc1b069c6fa47ba2f7b1c56
Charlie Newlinds <cnewlinds@gmail.com>
2024-03-30 13:38:07 +1100

    add tatto library

--- tailwind-project/src/components/Library.jsx
+++ tailwind-project/src/components/Library.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import placeHolderImg from "../assets/Placeholder.jpg"; 
+ 
+
 
 function Library ()  {
     return (
         <div className="text-center border border-solid border-black h-full rounded-md bg-white">
-            <script src="https://cdn.babylonjs.com/viewer/babylon.viewer.js"></script>
             <h1>Tattoo Library</h1>
             <div className="mt-2">
                 <input
@@ -11,6 +13,11 @@ function Library ()  {
                     placeholder="Search..."
                     className="px-2 py-1 border border-solid border-grey-300 rounded-md"
                 />
+            </div>
+            <div className="grid sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-2 px-2 pt-2">
+                {[...Array(10)].map((_, index) => ( // Loop to create 10 images
+                    <img key={index} className="rounder-md" src={placeHolderImg} width="full" loading="lazy" alt={`Image ${index + 1}`} />
+                ))}
             </div>
         </div>
     )
